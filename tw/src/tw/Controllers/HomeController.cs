41fed6ab13ce7040ab493d2a6439729564ee7c01
Rick Anderson <a.ricka00@gmail.com>
2014-12-03 17:54:41 -0800

    initial commit

---
+++ tw/src/tw/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using MvcSample.Filters; 
+
+namespace tw.Controllers
+{
+  [UseStopwatch]
+  public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+    [Delay(500)]
+    public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
